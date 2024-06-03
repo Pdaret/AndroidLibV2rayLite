b836b0aad848da22a7c617d4d89d92b3177fb017
Pdaret <mr.srezaei@outlook.com>
2024-06-03 15:56:43 +0330

    fix: :sparkles: adding server key

--- keys/libv2ray_keys.go
+++ keys/libv2ray_keys.go
@@ -1,7 +1,7 @@
 package keys
 
 const APP_KEY = "303a223afec7112541253a81c1043b6107005b3a2856921ca503bd5c7d6c5163"
-const SERVER_KEY = ""
+const SERVER_KEY = "36fb63a6dbb3bef7b6e4470a0bd06e97a187f579d2d6448d45a7bde38d135f8a"
 
 type KeyService interface {
 	GetAppKey() string
