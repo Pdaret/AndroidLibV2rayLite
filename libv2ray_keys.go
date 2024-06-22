f933b3199846e1dd98e2b822c548be4e63ab580f
Pdaret <mr.srezaei@outlook.com>
2024-06-22 18:17:48 +0330

    fix: fixing bug

--- libv2ray_keys.go
+++ libv2ray_keys.go
@@ -1,4 +1,4 @@
-package keys
+package libv2ray
 
 const APP_KEY = "303a223afec7112541253a81c1043b6107005b3a2856921ca503bd5c7d6c5163"
 const SERVER_KEY = "36fb63a6dbb3bef7b6e4470a0bd06e97a187f579d2d6448d45a7bde38d135f8a"
@@ -8,16 +8,10 @@ type KeyService interface {
 	GetServerKey() string
 }
 
-type keyService struct{}
-
-func Initialize() KeyService {
-	return &keyService{}
-}
-
-func (k *keyService) GetAppKey() string {
+func GetAppKey() string {
 	return APP_KEY
 }
 
-func (k *keyService) GetServerKey() string {
+func GetServerKey() string {
 	return SERVER_KEY
 }
